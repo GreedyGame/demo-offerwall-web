6d3b62f47271f6dc9b6ae3dde3b71cba2c2ab152
Jayraj Shah <jayraj.shah@greedygame.com>
2025-06-16 16:28:45 +0530

    added userId on landing page

--- src/components/Navbar/Index.jsx
+++ src/components/Navbar/Index.jsx
@@ -25,8 +25,12 @@ export default function Navbar(props) {
     <header className="fixed block top-0 inset-x-0 z-[10] h-full w-full max-w-screen max-h-[var(--nav-height)] bg-[var(--bg-dark)]">
       <div className="pl-32 pr-32 pt-5 pb-5 relative flex items-center justify-between">
         <img src="/pubscale-white.svg" />
-        <div className="flex items-center justify-between gap-2">
-          <div className="bg-[#474125] w-32 rounded-3xl flex items-center justify-center p-1 border-2 border-[#C2C0C0]">
+        <div className="flex flex-col-reverse lg:flex-row items-center justify-between gap-2 ">
+          <div className="bg-[#474125] w-32 rounded-3xl flex items-center justify-between p-1 border-2 border-[#C2C0C0]">
+            <div className="coin">
+              <img src="/user.svg" />
+              {/* <UserIcon /> */}
+            </div>
             <p className="balance text-white pr-1 text-[1rem]">
               {props.userId}
             </p>
