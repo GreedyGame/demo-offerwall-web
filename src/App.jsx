1a330ff213f8edfd7fb7eedb726bfc44cf010820
Jayraj Shah <jayraj.shah@greedygame.com>
2025-06-16 16:29:16 +0530

    Added Placeholders for uid and aid

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import Landing from "./components/Landing";
 import Navbar from "./components/Navbar/Index";
 
-const USER_ID = "test_nnn";
+const USER_ID = "<your_user_id>";
+const APP_ID = "<your_app_id>";
 
 function App() {
   return (
     <div>
       <Navbar userId={USER_ID} />
-      <Landing userId={USER_ID} />
+      <Landing userId={USER_ID} appId={APP_ID} />
     </div>
   );
 }
